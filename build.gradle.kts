a64de26a7816ff025b0780b46e38e92efbeff54a
traumarouge <traumarouge@gmail.com>
2019-09-03 07:43:50 +0200

    Add initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+version = "0.0.1-SNAPSHOT"
+
+tasks.withType<JavaCompile> {
+    with(options) {
+        compilerArgs.add("-Xlint:deprecation")
+        compilerArgs.add("-Xlint:unchecked")
+        compilerArgs.add("-parameters")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.1")
+    testImplementation("org.assertj:assertj-core:3.13.2")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "marsrover-vanilla"
